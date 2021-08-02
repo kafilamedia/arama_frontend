1499d8ae57da9ea46a1361611c5f190e364c8de2
Fajar AM <somabangsa@gmail.com>
2021-08-02 19:43:02 +0700

    show result get rapor data

--- src/component/pages/report/StudentSemesterReport.tsx
+++ src/component/pages/report/StudentSemesterReport.tsx
@@ -8,9 +8,18 @@ import Class from './../../../models/Class';
 import WebResponse from './../../../models/commons/WebResponse';
 import FormGroup from '../../form/FormGroup';
 import AttachmentInfo from './../../../models/settings/AttachmentInfo';
+import { tableHeader } from './../../../utils/CollectionUtil';
 
 class State {
     classes:Class [] = [];
+    items:ReportItem[] = [];
+}
+interface ReportItem {
+    name:string, class:string,
+    categories:CategoryResult[]
+}
+interface CategoryResult {
+    name:string, total_point:number, predicate_letter:string, predicate_desc:string
 }
 class StudentSemesterReport extends BasePage
 {
@@ -40,7 +49,7 @@ class StudentSemesterReport extends BasePage
         const classId = this.selectClassRef.current.value;
         this.commonAjax(
             this.studentService.getRaporData,
-            console.debug,
+            this.raporDataLoaded,
             this.showCommonErrorAlert,
             classId
         )
@@ -75,9 +84,24 @@ class StudentSemesterReport extends BasePage
     classesLoaded = (response:WebResponse) => {
         this.setState({classes: response.items});
     }
+    raporDataLoaded = (response:WebResponse) => {
+        this.setState({items: response.items});
+    }
+    get reportHeader() {
+        const headers = ['No', 'Siswa', 'Kelas']
+        const items = this.state.items;
+        if (items.length == 0) return [];
 
+        items[0].categories.forEach((cat)=>{
+            headers.push(cat.name);
+            headers.push("Predikat");
+            headers.push("Keterangan");
+        });
+        return headers;
+    }
     render() {
         const classes = this.state.classes;
+
         return (
             <div className="container-fluid section-body">
                 <h2>{this.title}</h2>
@@ -94,6 +118,35 @@ class StudentSemesterReport extends BasePage
                         <a className="btn btn-link ml-2" onClick={this.downloadData} >Download Detail Data</a>
                     </FormGroup>
                 </form>
+                {this.state.items.length == 0?
+                <div className="alert alert-warning">Tidak ada data</div>:
+                <div className="mt-5 bg-white border border-secondary" style={{overflow: 'scroll'}}>
+                    <table className="table table-striped">
+                        {tableHeader(...this.reportHeader)}
+                        <tbody>
+                            {this.state.items.map((item:ReportItem, i:number) => {
+
+                                return (
+                                    <tr key={`r_i_${item.name}`} >
+                                        <td>{i+1}</td>
+                                        <td>{item.name}</td>
+                                        <td>{item.class}</td>
+                                        {item.categories.map((c:CategoryResult, ci:number)=>{
+                                            return (
+                                                <React.Fragment key={"F"+ci+i}>
+                                                    <td>{c.total_point}</td>
+                                                    <td children={c.predicate_letter}/>
+                                                    <td children={c.predicate_desc} />
+                                                </React.Fragment>
+                                            )
+                                        })}
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+                }
             </div>
         )
     }
