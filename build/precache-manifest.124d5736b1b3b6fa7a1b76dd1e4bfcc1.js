8dd847fb8f5cd0de3e660f4fbd425bff114a3d96
Fajar AM <somabangsa@gmail.com>
2022-12-11 20:10:12 +0700

    build

---
+++ build/precache-manifest.124d5736b1b3b6fa7a1b76dd1e4bfcc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff863b8df42dafa50806299368bb7c51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ae77b19ddd34f756b78",
+    "url": "/static/css/main.2eafcf1b.chunk.css"
+  },
+  {
+    "revision": "a945ae4bcfba5c7cc21b",
+    "url": "/static/js/2.27dfda6a.chunk.js"
+  },
+  {
+    "revision": "c475aad6c8086443b75917eb07754d10",
+    "url": "/static/js/2.27dfda6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ae77b19ddd34f756b78",
+    "url": "/static/js/main.1aadc420.chunk.js"
+  },
+  {
+    "revision": "a1a8957f32543079aee6",
+    "url": "/static/js/runtime-main.7064a2d2.js"
+  }
+]);
